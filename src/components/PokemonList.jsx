fb004429f20a7066c33c1563f1ae6b22da30fba2
Daniel Ferney Hernandez Galvis <ferneyhernandez10@gmail.com>
2023-11-07 09:58:54 -0500

    -	Se implementa filtrado de Pokemones por nombre en el input (al buscar pi retorna los Pokemones como Pikachu). -	La lista de los Pokemon se puede ver cada uno en su Card independiente. -	Se agrega una imagen al Componente PokemonList. -	Descripción de la página y de las tecnologías que se han aplicado en el proyecto.

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import pokemonImage from "../image/pokemonImage.jpg";
 
 export const PokemonList = () => {
   const [pokemons, setPokemons] = useState([]);
+  const [pokemonSearch, setPokemonSearch] = useState("");
 
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=100&offset=0#")
@@ -10,16 +12,68 @@ export const PokemonList = () => {
       .then((pokemons) => setPokemons(pokemons.results));
   }, []);
 
-  const pokemonsList = pokemons?.map((pokemon, index) => (
-    <li className="list-decimal" key={index}>
+  const handleChange = (event) => {
+    setPokemonSearch(event.target.value);
+  };
+
+  let leakedPokemonName = [];
+  if (!pokemonSearch) {
+    leakedPokemonName = pokemons;
+  } else {
+    leakedPokemonName = pokemons.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(pokemonSearch.toLocaleLowerCase())
+    );
+  }
+
+  const pokemonsList = leakedPokemonName?.map((pokemon, index) => (
+    <div
+      className="block max-w-sm px-44 py-3 m-1 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+      key={index}
+    >
       <Link to={`/information/${pokemon.name}`}>{pokemon.name}</Link>
-    </li>
+    </div>
   ));
 
   return (
-    <div className="flex flex-col flex-wrap justify-center items-center">
-      <p className="text-lg font-medium capitalize">Pokemon List</p>
-      <p className="capitalize">{pokemonsList}</p>
+    <div className="mx-20">
+      <p className="text-4xl text-center font-bold capitalize">Pokemon List</p>
+      <div className="flex flex-row">
+        <img src={pokemonImage} alt="Loading..." />
+        <div className="flex flex-col">
+          <h5 className="text-2xl font-medium">
+            - Explanation of the pokemon page
+          </h5>
+          <p className="text-lg">
+            This application is designed so that you can find at the beginning a
+            list of one hundred Pokémon which has a search engine with
+            functionality that depending on what you type, a select list of
+            Pokémon appears. When you click on the name of the Pokémon you
+            choose, it will be sent to another tab where you will find the name,
+            the image, some of the characteristics that identify it, among them
+            we can find: Skills, Movements, Statistics and Types.
+          </p>
+          <h5 className="text-2xl font-medium">
+            - Explanation of the technologies that have been applied in the
+            project
+          </h5>
+          <p className="text-lg">
+            The technologies that have been used to create this project have
+            been React with which we have brought the information from the
+            hundred pokemon app and it has been put into functionality and with
+            Tailwind CSS we have helped us with the design.
+          </p>
+        </div>
+      </div>
+      <input
+        type="text"
+        placeholder="Pokemon Search"
+        value={pokemonSearch}
+        onChange={handleChange}
+        className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      />
+      <div className="flex flex-wrap justify-between text-2xl font-medium capitalize">
+        {pokemonsList}
+      </div>
     </div>
   );
 };
